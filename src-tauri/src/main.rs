6883081ca7fe8fed7031962922f5b1317c327e93
southorange1228 <15280970040@163.com>
2022-10-11 20:43:37 +0800

    feat: update config

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,16 @@
     windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+mod cmd;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let desc = app.package_info().description;
+            println!("{}", desc);
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![cmd::greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
