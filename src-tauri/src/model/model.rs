537dff9fdf5b44ef5b8469ddab9856c82ca09dfd
southorange1228 <southorange1228@foxmail.com>
2022-10-21 10:14:29 +0800

    feat: update sqlite table struct

--- src-tauri/src/model/model.rs
+++ src-tauri/src/model/model.rs
@@ -1,14 +1,27 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug,Serialize,Deserialize)]
-pub struct Record {
+pub struct MemoData {
     id: String,
-    tag: String,
+    title: String,
     content: String,
+    created: u64,
+    updated: u64,
 }
 
 #[derive(Debug,Serialize,Deserialize)]
-pub struct Tag {
+pub struct MemoTag {
     id: String,
-    content: String,
+    name: String,
+    created: u64,
+    updated: u64,
+}
+
+#[derive(Debug,Serialize,Deserialize)]
+pub struct MemoTagData {
+    id: String,
+    tag_id: String,
+    memo_id:String,
+    created: u64,
+    updated: u64,
 }
